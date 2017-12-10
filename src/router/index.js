b6a44c9abfd8c9c23e4d180cea8e82758d8b5bc5
owenrauckman <owenrauckman@gmail.com>
2017-12-10 16:45:17 -0600

    Adds mortgage redirect

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ Vue.use(Router);
 export default new Router({
   mode: 'history',
   routes: [
-    { path: '/', component: App, alias: '/mortgage' },
+    { path: '/', component: App, redirect: '/mortgage' },
   ],
 
 });
